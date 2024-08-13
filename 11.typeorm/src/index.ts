e4a2bbd4fa11cbaec5f6284484f5e4bb9074aa59
totoro <totoro.fu@webeye.com>
2024-08-13 11:31:40 +0800

    11.typeorm

--- 11.typeorm/src/index.ts
+++ 11.typeorm/src/index.ts
@@ -1,25 +1,16 @@
 import { AppDataSource } from "./data-source";
 import { User } from "./entity/User";
-//初始化数据源
+import { Profile } from "./entity/Profile";
 AppDataSource.initialize().then(async () => {
-  //select user.firstName,user.lastName from user;
-  //创建一个针对User实合格的查询 构建器，并将该实体命名为user
-  const queryBuilder = AppDataSource.manager.createQueryBuilder();
-  //选择查询中返回的字段
-  queryBuilder.select(["user.firstName", "user.lastName"]);
-  //添加更多的字段
-  queryBuilder.addSelect("user.age");
-  //指定查询的主表
-  queryBuilder.from(User, "user");
-  //通过where指定查询条件
-  queryBuilder.where("user.id = :id", { id: 4 });
-  //还可以添加额外的条件
-  queryBuilder.andWhere("user.isActive=:isActive", { isActive: true });
-  //指定要返回一条还是多条
-  const user = await queryBuilder.getOne();
-  const users = await queryBuilder.getMany();
-  console.log("user", user);
-  console.log("users", users);
+  const newUser = new User();
+  newUser.firstName = "2";
+  newUser.lastName = "2";
+  newUser.age = 1;
+  newUser.email = "2@qq.com";
+  //await AppDataSource.manager.save(newUser);
 
-  await queryBuilder.delete().from(User).where("id=:id", { id: 4 }).execute();
+  const newPofile = new Profile();
+  newPofile.bio = "2的个人介绍";
+  newPofile.user = newUser;
+  await AppDataSource.manager.save(newPofile);
 });
